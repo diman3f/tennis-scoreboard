66c2652ad1e3312c79e86b1e77ec1ce3567bbdad
DimanT <tyukilin555@gmail.com>
2025-10-01 20:42:18 +0400

    chore: добавлен метод получения игрока по имени

--- src/main/java/com/diman_3f/tennis_scoreboard/dao/PlayerDao.java
+++ src/main/java/com/diman_3f/tennis_scoreboard/dao/PlayerDao.java
@@ -2,10 +2,12 @@ package com.diman_3f.tennis_scoreboard.dao;
 
 import com.diman_3f.tennis_scoreboard.entity.Player;
 import com.diman_3f.tennis_scoreboard.context.UtilSessionFactory;
+import jakarta.persistence.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class PlayerDao {
 
@@ -20,13 +22,32 @@ public class PlayerDao {
             return player;
         }
     }
+
     public List<Player> findPlayers() {
-        try(Session session = UtilSessionFactory.getSession()) {
+        try (Session session = UtilSessionFactory.getSession()) {
             Transaction transaction = session.beginTransaction();
             List<Player> players = session.createQuery("from Player", Player.class).getResultList();
             transaction.commit();
             return players;
         }
+    }
+
+    public Player findByName(String namePlayer) {
+        try (Session session = UtilSessionFactory.getSession()) {
+            Transaction transaction = session.beginTransaction();
+
+            String hql = "from Player player where player.name=:name_player";
+            Query query = session.createQuery(hql, Player.class);
+            query.setParameter("name_player", namePlayer);
+            List<Player> list = query.getResultList();
+            transaction.commit();
+            for (Player player : list) {
+                if (player.getName().equals(namePlayer)) {
+                    return player;
+                }
+            }
+        }
+        throw new NoSuchElementException(String.format("Игрок с именем %s", namePlayer + " не найден"));
     }
 
 }
