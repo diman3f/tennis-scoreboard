23b3700ff0a49e2d784f248399cdc291ec736157
DimanT <tyukilin555@gmail.com>
2025-10-14 21:50:31 +0400

    refactor: возвращаю UUID при создании матча

--- src/main/java/com/diman_3f/tennis_scoreboard/services/MatchCreatorService.java
+++ src/main/java/com/diman_3f/tennis_scoreboard/services/MatchCreatorService.java
@@ -1,57 +1,61 @@
 package com.diman_3f.tennis_scoreboard.services;
 
-import com.diman_3f.tennis_scoreboard.entities.Player;
 import com.diman_3f.tennis_scoreboard.dao.PlayerDao;
 import com.diman_3f.tennis_scoreboard.models.ActiveMatch;
-import com.diman_3f.tennis_scoreboard.models.ScorePlayer;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 /**
- Класс для создания новых матчей для учета счета игры.
-
- Функциональность:
- Создает матч на основе переданных имен игроков из vue
- Выдать матч по uuid
-
- Взаимодействие с другими классами программы:
- Контроллер получает доступ к активному матчу через uuid матча
- Через контроллер передаем id игрока для начисления point
-
+ * Класс для создания новых матчей для учета счета игры.
+ * <p>
+ * Функциональность:
+ * Создает матч на основе переданных имен игроков из vue
+ * Выдать матч по uuid
+ * <p>
+ * Взаимодействие с другими классами программы:
+ * Контроллер получает доступ к активному матчу через uuid матча
+ * Через контроллер передаем id игрока для начисления point
  */
 public class MatchCreatorService {
 
     private PlayerDao playerDao;
     private Map<UUID, ActiveMatch> matches;
-    private UUID uuid;
 
 
-    public MatchCreatorService(PlayerDao playerDao) {
-        this.playerDao = playerDao;
+    public MatchCreatorService() {
         this.matches = new HashMap<>();
     }
 
-    public void createCurrentMatch(String namePlayer1, String namePlayer2) {
+    public void setPlayerDao(PlayerDao playerDao) {
+        this.playerDao = playerDao;
+    }
 
-        Player playerOne = playerDao.findByName(namePlayer1);
-        Player playerTwo = playerDao.findByName(namePlayer2);
+    public UUID createCurrentMatch(String namePlayer1, String namePlayer2) {
 
-            UUID uuid = UUID.randomUUID();
-            this.uuid = uuid;
-            ActiveMatch activeMatch = ActiveMatch.builder()
-                    .id(uuid)
-                    .playerOneID(playerOne.getId())
-                    .playerTwoID(playerTwo.getId())
-                    .build();
-            matches.put(uuid, activeMatch);
-        }
+        int idPlayerOne = playerDao.findByName(namePlayer1).getId();
+        int idPlayerTwo = playerDao.findByName(namePlayer2).getId();
+
+        UUID uuid = UUID.randomUUID();
+        ActiveMatch activeMatch = ActiveMatch.builder()
+                .playerOneID(idPlayerOne) //todo варик заменить на имена, чтобы не делать лишние запросы сверху
+                .playerTwoID(idPlayerTwo) //todo еще варик создать отдельный класс для инициализации ActiveMatch (OngoingMatch)
+                .isActive(true)
+                .isTieBreak(false)
+                .isGameEquals(false)
+                .build();
+        matches.put(uuid, activeMatch);
+        return uuid;
+    }
 
     public ActiveMatch getMatch(UUID uuid) {
         return matches.get(uuid);
     }
 
-    public UUID getUuid() {
-        return uuid;
+    public void deleteMatchByUUID(UUID uuid) {
+        matches.remove(uuid);
     }
+
 }
 
