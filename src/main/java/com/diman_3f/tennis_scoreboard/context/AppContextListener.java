d569edfdea4c3c1b20d1ab133444eff645e5d1fa
DimanT <tyukilin555@gmail.com>
2025-11-26 22:48:32 +0400

    добавил регистрация сервис для общего доступа

--- src/main/java/com/diman_3f/tennis_scoreboard/context/AppContextListener.java
+++ src/main/java/com/diman_3f/tennis_scoreboard/context/AppContextListener.java
@@ -1,2 +1,17 @@
-package com.diman_3f.tennis_scoreboard.context;public class AppContextListener {
+package com.diman_3f.tennis_scoreboard.context;
+
+import com.diman_3f.tennis_scoreboard.dao.PlayerDao;
+import com.diman_3f.tennis_scoreboard.services.MatchCreatorService;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+
+@WebListener
+public class AppContextListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        ServiceLocator.registerService(new PlayerDao());
+        ServiceLocator.registerService(new MatchCreatorService());
+
+    }
 }
