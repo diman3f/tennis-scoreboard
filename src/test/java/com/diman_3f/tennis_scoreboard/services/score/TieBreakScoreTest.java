b9601c10565e3d1c6bba1e202ccd04c04d4030c8
DimanT <tyukilin555@gmail.com>
2026-02-24 19:19:57 +0400

    refactor: удалил не актуальные тесты, добавил тесты на проверку сетов и завершение матча

--- src/test/java/com/diman_3f/tennis_scoreboard/services/score/TieBreakScoreTest.java
+++ src/test/java/com/diman_3f/tennis_scoreboard/services/score/TieBreakScoreTest.java
@@ -3,12 +3,13 @@ package com.diman_3f.tennis_scoreboard.services.score;
 import com.diman_3f.tennis_scoreboard.dto.ScoreDto;
 import com.diman_3f.tennis_scoreboard.entities.Player;
 import com.diman_3f.tennis_scoreboard.models.OngoingMatch;
+import com.diman_3f.tennis_scoreboard.services.MatchScoreCalculationService;
+import com.diman_3f.tennis_scoreboard.services.TennisRuleHandler;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class RegularScoreTest {
-
+public class TieBreakScoreTest {
     OngoingMatch match;
     private final int INDEX_ONE_PLAYER = 0;
     private final int INDEX_OPPOSITE_PLAYER = 1;
@@ -21,45 +22,42 @@ public class RegularScoreTest {
     }
 
     @Test
-    public void shouldPlayerWonGame_WhenWonFourPoints() {
+    public void shouldOnePlayerWonSet_WhenOnePlayerWonSevenPoint() {
+        setScoreGameSixSix();
+        for (int i = 0; i < 7; i++) {
+            match.upPointPlayer(INDEX_ONE_PLAYER);
+        }
+        ScoreDto score = match.getScore();
+        Assertions.assertEquals(score.getSetOne(), 1);
+    }
+
+    @Test
+    public void shouldOnePlayerWonSet_WhenDifferentPointTwo(){
+        setScoreGameSixSix();
+        setScorePointSixSix();
+        match.upPointPlayer(INDEX_ONE_PLAYER);
+        match.upPointPlayer(INDEX_ONE_PLAYER);
+        ScoreDto score = match.getScore();
+        Assertions.assertEquals(score.getSetOne(), 1);
+    }
+
+    private void setScoreGameSixSix() {
+        for (int i = 0; i < 23; i++) {
+            match.upPointPlayer(INDEX_ONE_PLAYER);
+        }
+        for (int i = 0; i < 25; i++) {
+            match.upPointPlayer(INDEX_OPPOSITE_PLAYER);
+        }
         for (int i = 0; i < 4; i++) {
             match.upPointPlayer(INDEX_ONE_PLAYER);
         }
-        ScoreDto score = match.getScore();
-        Assertions.assertEquals(1, score.getGameOne());
     }
 
-    @Test
-    public void shouldAdvantageOnePlayer_WhenScoreFortyFortyOnePlayerWonPoint() {
-        changeScoreOnForty();
-        match.upPointPlayer(INDEX_ONE_PLAYER);
-        ScoreDto score = match.getScore();
-        Assertions.assertEquals("AD", score.getPointOne());
-    }
-
-    @Test
-    public void shouldFortyOnePlayer_WhenScoreAdvantageOnePlayerAndTwoPlayerWinPoint() {
-        changeScoreOnForty();
-        match.upPointPlayer(INDEX_ONE_PLAYER);
-        match.upPointPlayer(INDEX_OPPOSITE_PLAYER);
-        ScoreDto score = match.getScore();
-        Assertions.assertEquals("40", score.getPointOne());
-    }
-
-    @Test
-    public void shouldOnePlayerWonGame_WhenScoreOnePlayerAdvantage() {
-        changeScoreOnForty();
-        match.upPointPlayer(INDEX_ONE_PLAYER);
-        match.upPointPlayer(INDEX_ONE_PLAYER);
-        ScoreDto score = match.getScore();
-        Assertions.assertEquals(1, score.getGameOne());
-    }
-
-    private void changeScoreOnForty() {
-        for (int i = 0; i < 3; i++) {
+    private void setScorePointSixSix(){
+        for (int i = 0; i < 6; i++) {
             match.upPointPlayer(INDEX_ONE_PLAYER);
         }
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 6; i++) {
             match.upPointPlayer(INDEX_OPPOSITE_PLAYER);
         }
     }
